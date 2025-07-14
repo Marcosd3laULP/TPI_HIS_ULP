b8d2188224fb9ad77c52c627433e5d05bd3b59d5
Marcosd3laULP <usuario4ltnum1@gmail.com>
2025-07-13 22:58:23 -0300

    Progreso en la evaluacion preliminar

--- Control/enfermeroControl.js
+++ Control/enfermeroControl.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { Prestador } = require("../Modelo/relaciones/asociaciones");
 const { Paciente } = require("../Modelo/relaciones/asociaciones");
 const { Atenciones } = require('../Modelo/relaciones/asociaciones');
@@ -6,3 +7,14 @@ const { Internacion } = require("../Modelo/relaciones/asociaciones");
 exports.mostrarOpEnfermero = async function (req, res) {
     res.render("enfermeria/seccionEnf");
 };
+
+
+exports.buscarTodoEnfermero = async function (req, res) {
+    try {
+        const enfermero =  await Prestador.findAll({where:{ Rol: 'Enfermero'}}) 
+        res.render("enfermeria/listaInternosEval", { enfermeros: enfermero})
+    } catch (error) {
+        console.error("Error al buscar enfermeros:", error.message);
+        res.status(500).send("Error al cargar la vista de evaluación.");
+    }
+}
